a72e8983f5bb4cf97ed9d6c0df47cb39ac6cd1d4
Sergey K <servlakur@gmail.com>
2025-04-15 14:15:06 +0300

    Подготовка js и html для добавления документов к заявке

--- crm/static/crm/js/files-gallery.js
+++ crm/static/crm/js/files-gallery.js
@@ -1,15 +1,44 @@
 $(document).ready(function () {
-    console.log("Скрипт для файловой галереи")
+    // Открытие collapseRequestDocsList при загрузке страницы
+    $('.btn-for-collapse-request-docs').click();
 
     // Смена текста docs-list-title при просмотре документов
     $("#collapseRequestDocsList").on('show.bs.collapse', function(e) {
-        console.log("Открыт колапс с доками")
-        $('.docs-list-title').text('Скрыть документы')
+        $('.docs-list-title').html(`Скрыть документы <i class="bi bi-arrow-up-circle"></i>`)
     })
 
     // Смена текста docs-list-title при закрытии просмотра документов
     $("#collapseRequestDocsList").on('hidden.bs.collapse', function(e) {
-        console.log("Открыт колапс с доками")
-        $('.docs-list-title').text('Посмотреть документы')
+        $('.docs-list-title').html(`Посмотреть документы <i class="bi bi-arrow-down-circle"></i>`)
     })
+
+    // Запрос формы для добавления файлов к заявке
+    $('.add-files-button').click(function (e) { 
+        e.preventDefault();
+        var filesType = $(this).data('add-item-type');
+        var urlForGetModalAddItemsForm = $(this).data('url-for-add-item');
+        var serviceRequestId = $(this).data('service_req_id')
+
+            $.ajax({
+                type: "GET",
+                url: urlForGetModalAddItemsForm,
+                data: {
+                    "files_type": filesType,
+                    "service_request_id": serviceRequestId
+                
+                },
+                dataType: "json",
+                success: function (response) {
+                    console.log("Пришел ответ от бэка")
+                    var modalForAddItemsForm = response.new_content
+                    $('.main_wrapper').prepend(modalForAddItemsForm);
+                    $('#addFileModal').modal('show');
+                }
+            });
+    });
+
+    // При закрытии модального окна для для добавления файлов, удаляем окно целиком из DOM    
+    $(document).on('hidden.bs.modal', '#addFileModal', function (e) {
+        $('#addFileModal').remove();
+    });
 });
