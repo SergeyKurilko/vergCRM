76053f47505a019b39d90c00a3fc1a8e9a6ac776
Sergey K <servlakur@gmail.com>
2025-04-21 10:33:31 +0300

    added ReminderViewSet (method 'destroy')

--- api/view_sets.py
+++ api/view_sets.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
 
-# Create your views here.
+from api.serializers import ReminderSerializer
+from crm.models import Reminder
+
+class ReminderViewSet(ModelViewSet):
+    queryset = Reminder.objects.all()
+    serializer_class = ReminderSerializer
+
+    # TODO: Добавить permission_class
+
+    def destroy(self, request, *args, **kwargs):
+        """Ожидает метод delete"""
+        instance = self.get_object()
+        instance.is_active = False
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
